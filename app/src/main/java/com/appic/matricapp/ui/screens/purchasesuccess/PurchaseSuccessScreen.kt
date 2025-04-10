5c4c6a0a2d50185ab1818ead60cbf0c6c5d926e3
Bacsa Attila <attila.bacsa@hiflylabs.com>
2025-04-10 16:31:16 +0200

    Finished PurchaseSuccessScreen

--- app/src/main/java/com/appic/matricapp/ui/screens/purchasesuccess/PurchaseSuccessScreen.kt
+++ app/src/main/java/com/appic/matricapp/ui/screens/purchasesuccess/PurchaseSuccessScreen.kt
@@ -5,18 +5,22 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.End
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.appic.matricapp.R
+import com.appic.matricapp.ui.theme.MatricAppTheme
 import com.appic.matricapp.ui.theme.MatricappLime
 
 @Composable
@@ -25,27 +29,37 @@ fun PurchaseSuccessScreen(onClose: () -> Unit) {
         modifier = Modifier
             .fillMaxSize()
             .background(MatricappLime)
+            .padding(bottom = dimensionResource(R.dimen.dp_24))
     ) {
         Image(
-            painter = painterResource(id = R.drawable.ic_confetti_png),
+            painter = painterResource(R.drawable.ic_confetti_png),
             contentDescription = "",
             modifier = Modifier.fillMaxWidth(),
             contentScale = ContentScale.Crop
         )
+
         Text(
             text = stringResource(R.string.vignettes_purchased_successfully),
-            modifier = Modifier.padding(start = dimensionResource(R.dimen.dp_16)),
+            modifier = Modifier.padding(horizontal = dimensionResource(R.dimen.dp_24)),
             style = typography.displayLarge
         )
+
         Image(
-            painter = painterResource(id = R.drawable.ic_character_png),
-            contentDescription = ""
+            painter = painterResource(R.drawable.ic_character_png),
+            contentDescription = "",
+            modifier = Modifier
+                .weight(1f)
+                .align(End)
+                .offset(x = dimensionResource(R.dimen.dp_32))
+                .padding(vertical = dimensionResource(R.dimen.dp_16)),
+            contentScale = ContentScale.Crop
         )
+
         Button(
             onClick = onClose,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = dimensionResource(R.dimen.dp_20))
+                .padding(horizontal = dimensionResource(R.dimen.dp_24))
         ) {
             Text(
                 text = stringResource(R.string.all_right),
@@ -54,5 +68,13 @@ fun PurchaseSuccessScreen(onClose: () -> Unit) {
                 style = typography.titleSmall
             )
         }
+    }
+}
+
+@Composable
+@Preview
+private fun PurchaseSuccessScreenPreview() {
+    MatricAppTheme {
+        PurchaseSuccessScreen {}
     }
 }
